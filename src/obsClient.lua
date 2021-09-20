2c6f600ea3cdc6b40c874891fcb3a206578b5ec3
scawp <rob@scawp.com>
2021-09-20 23:24:38 +0200

    add obsclient.lua currently only logs obs events

---
+++ src/obsClient.lua
@@ -0,0 +1,52 @@
+local obs_client = require("websocket").new("localhost", 6666)
+
+obs_client.init = function(self)
+  self.decode = require('decoder')
+  self.encode = require('encoder')
+  --self:load_apis()
+
+  self.connected = false
+end
+
+obs_client.load_apis = function()
+  --obs_client.scenes = require "src.api.requests.scenes"
+end
+
+--update already in the require, uncomment if needs extending
+--obs_client.update = function(self, dt)
+  --self:update()
+--end
+
+obs_client.onopen = function(self)
+  self.connected = true
+  self:log("Connection to OBS opened")
+end
+
+obs_client.onmessage = function(self, msg)
+  self:log("--------------")
+  self:log(msg)
+
+
+  local json_msg = self.decode(msg)
+  self:log(json_msg)
+
+  --process msg
+
+end
+
+obs_client.onclose = function(self)
+  self.connected = false
+  self:log("Connection to OBS closed")
+end
+
+obs_client.debug = {
+  init_time = os.time()
+}
+
+obs_client.log = function(self, data)
+  print(data)
+  love.filesystem.append("obsClient-" .. self.debug.init_time .. ".log", 
+                         "[".. os.time() .. "] obs_client: " .. tostring(data) .. "\r\n")
+end
+
+return obs_client
